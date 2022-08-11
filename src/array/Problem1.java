2bbc32dd43ec1c0ea2c41edddb1e1724a5298366
M Haque <mdmotahharul.haque@gmail.com>
2022-08-12 04:24:25 +0530

    prefix sum problem

--- src/array/Problem1.java
+++ src/array/Problem1.java
@@ -2,6 +2,7 @@ package array;
 public class Problem1 {
     public static int getSum(int[] prefixSum, int l, int r)
     {
+        //process query using these logics
         if (l != 0) {
             return prefixSum[r] - prefixSum[l - 1];
         }
@@ -11,9 +12,13 @@ public class Problem1 {
     }
     public static void main(String[] args) {
         int[] arr = {2, 10, 13, 22, 28, 33, 37};
+        //create array of same length w.r.t arr
         int[] prefixSum = new int[arr.length];
+        //initialize 0th index with the oth index of arr[0]
         prefixSum[0] = arr[0];
 
+        //populate prefixSum array using below logic
+        //i.e. pre-calculation of sum
         for (int i = 1; i < arr.length; i++) {
             prefixSum[i] = prefixSum[i - 1] + arr[i];
         }
